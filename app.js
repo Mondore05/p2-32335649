48d04eeb9dea2c6357024dea5c036ec69c8cc89e
Mondore05 <yolbertvillarroel2047@gmail.com>
2025-04-05 23:29:39 -0400

    primer commit lol

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+
+app.get('/',(req, res)=>{
+    res.send('hola xd')
+})
+app.listen(3000, ()=> {
+    console.log('servidor a la espera')
+})
