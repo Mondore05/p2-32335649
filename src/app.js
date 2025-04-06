c3a7648b07b8dc382c801a93783e0376a765cf1a
Mondore05 <yolbertvillarroel2047@gmail.com>
2025-04-06 14:38:18 -0400

    ultimo commit

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
+//rutas
 const routes = require('./routes/index.routes')
 app.use(routes)
 
+//archivos estaticos 
+app.use((req, res) => {
+    res.sendFile(path.join(path.join(__dirname,'../pubic/index.html')))
+})
 app.listen(3000, ()=> {
     console.log('servidor a la espera')
 })
